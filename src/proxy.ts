8124066e06fa9f4bc73cebf5b7984231b36f57c9
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2026-07-12 19:31:27 +0200

    move proxy.ts to src folder

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/", "/(uk|en|pl)/:path*"],
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
